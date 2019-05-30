43d12fbec636004c2a33e1e5446f0bd2733b37c5
murata <murata@kmc.gr.jp>
2019-05-30 12:02:42 +0900

    atcoder template

--- atcoder/template.nim
+++ atcoder/template.nim
@@ -4,10 +4,16 @@ import sets,tables,intsets,queues
 template times*(n:int,body) = (for _ in 0..<n: body)
 template `max=`*(x,y) = x = max(x,y)
 template `min=`*(x,y) = x = min(x,y)
-proc scanf(formatstr: cstring){.header: "<stdio.h>", varargs.}
-proc scan(): int = scanf("%lld\n",addr result)
+proc getchar_unlocked():char {. importc:"getchar_unlocked",header: "<stdio.h>" ,discardable.}
+proc scan(): int =
+  while true:
+    let k = getchar_unlocked()
+    if k < '0' or k > '9': return
+    result = 10 * result + k.ord - '0'.ord
+
+# proc scanf(formatstr: cstring){.header: "<stdio.h>", varargs.}
+# proc scan(): int = scanf("%lld\n",addr result)
 # echo toSeq(0..10)
 # echo toSeq("123".items)
-
 let n = scan()
 let A = newSeqWith(n,scan())
