fadf6fc6c3f6291ae7a9ca72a3b37410ef56a112
murata <murata@kmc.gr.jp>
2019-07-24 13:34:00 +0900

    bitset

--- lib/cpp/bitset.nim
+++ lib/cpp/bitset.nim
@@ -1,23 +1,29 @@
-type CBitSet {.importcpp: "std::bitset", header: "<bitset>".} = object
-proc cInitBitSet(n:uint64 = 0): CBitSet {.importcpp: "std::bitset<64>(#)", nodecl.}
-# proc resize*[T](self:var CVector[T],n:int) {.importcpp: "#.resize(#)", nodecl.}
-# proc initVector*[T](size:int = 0): CVector[T] = ( result = cInitVector(T); result.resize(size))
-# proc size*[T](self: CVector[T]):int {.importcpp: "#.size()", nodecl.}
-# proc empty*[T](self: CVector[T]):bool {.importcpp: "#.empty()", nodecl.}
-# proc push_back*[T](self:var CVector[T],x:T) {.importcpp: "#.push_back(@)", nodecl.}
-# proc `[]`*[T](self: CVector[T],key:int): T {.importcpp: "#[#]", nodecl.}
-# proc `[]=`*[T](self:var CVector[T],key:int,val:T) {.importcpp: "#[#] = #", nodecl.}
-# proc clear*[T](self:var CVector[T]) {.importcpp: "#.clear()", nodecl.}
-# proc pop_back*[T](self:var CVector[T]) {.importcpp: "#.pop_back()", nodecl.}
+const BitSetSize = 64
+type CBitSet {.importcpp: "std::bitset<"&($BitSetSize)&">", header: "<bitset>".} = object
+proc initBitSet(n:uint): CBitSet {.importcpp: "std::bitset<"&($BitSetSize)&">(#)", nodecl.}
+# proc resize*(self:var CVector,n:int) {.importcpp: "#.resize(#)", nodecl.}
+# proc initVector*(size:int = 0): CVector = ( result = cInitVector(T); result.resize(size))
+# proc size*(self: CVector):int {.importcpp: "#.size()", nodecl.}
+# proc empty*(self: CVector):bool {.importcpp: "#.empty()", nodecl.}
+# proc push_back*(self:var CVector,x:T) {.importcpp: "#.push_back(@)", nodecl.}
+# proc `[]`*(self: CVector,key:int): T {.importcpp: "#[#]", nodecl.}
+# proc `[]=`*(self:var CVector,key:int,val:T) {.importcpp: "#[#] = #", nodecl.}
+# proc clear*(self:var CVector) {.importcpp: "#.clear()", nodecl.}
+# proc pop_back*(self:var CVector) {.importcpp: "#.pop_back()", nodecl.}
 # import sequtils # nim alias
-# proc add*[T](self:var CVector[T],x:T) = self.push_back(x)
-# proc len*[T](self:CVector[T]):int = self.size()
-# iterator items[T](self:CVector[T]): T = (for i in 0..<self.len: yield self[i])
-# proc toVector*[T](arr:seq[T]): CVector[T] = (result = initVector[T](arr.len);for i,a in arr: result[i] = a)
-# proc toSeq*[T](self:CVector[T]):seq[T] = self.mapIt(it)
-# proc `$`*[T](self:CVector[T]): string = $self.toSeq()
+# proc add*(self:var CVector,x:T) = self.push_back(x)
+# proc len*(self:CVector):int = self.size()
+# iterator items(self:CVector): T = (for i in 0..<self.len: yield self[i])
+# proc toVector*(arr:seq): CVector = (result = initVector(arr.len);for i,a in arr: result[i] = a)
+# proc toSeq*(self:CVector):seq = self.mapIt(it)
+type StdString {.importcpp: "std::string", header: "<string>".} = object
+proc c_str(a: StdString): cstring {.importcpp: "(char *)(#.c_str())".}
+proc to_string(self:CBitSet):StdString{.importcpp:"#.to_string()".}
+proc `$`*(self:CBitSet): cstring = self.to_string().c_str()
+
 
 when isMainModule:
   import unittest
-  test "C++ vector":
-    var b = cInitBitSet(10)
+  test "C++ bitset":
+    var b = initBitSet(64)
+    echo b
