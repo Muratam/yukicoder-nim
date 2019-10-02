5f9943683b80c1ce3c73437da1928dd8ffc135df
murata <murata@kmc.gr.jp>
2019-10-03 01:09:30 +0900

    dir

---
+++ lib/datastructure/heap/binarypatricia.nim
@@ -0,0 +1,157 @@
+import sequtils#,nimprof
+
+# 普通の2進表示のパトリシア木.
+# 葉同士がリンクし合うのと余分なノードはスキップするのとで高速.
+
+type
+  BinPatriciaNode = ref object
+    to0,to1 : BinPatriciaNode
+    count : int32 # (自身以下の木の)葉の要素数
+    isLeaf : bool
+    valueOrMask : int # 葉なら value / 枝なら一番下位bitが自身が担当するbit番号.それ以外はprefix
+  BinPatricia = ref object
+    root : BinPatriciaNode
+proc newBinPatriciaNode():BinPatriciaNode =
+  new(result)
+proc newBinPatricia(bitSize:int = 5):BinPatricia =
+  new(result)
+  result.root = newBinPatriciaNode()
+  result.root.count = 0
+  result.root.isLeaf = false
+  result.root.valueOrMask = 1 shl bitSize
+when NimMajor * 100 + NimMinor >= 18: import bitops
+else:
+  proc countLeadingZeroBits(x: culonglong): cint {.importc: "__builtin_clzll", cdecl.}
+  proc countTrailingZeroBits(x: culonglong): cint {.importc: "__builtin_ctzll", cdecl.}
+  proc fastLog2(x:culonglong):cint = 63 - countLeadingZeroBits(x)
+
+proc bitSize(self:BinPatriciaNode):int =
+  self.valueOrMask.culonglong.countTrailingZeroBits.int
+proc len*(self:BinPatricia) : int = self.root.count
+proc isTo1(self:BinPatriciaNode,n:int):bool{.inline.} =
+  (self.valueOrMask and n) == self.valueOrMask # 上位bitは同じはずなので
+
+# デバッグ用
+import strutils
+proc binary(x:int,fill:int=0):string = # 二進表示
+  if x == 0 : return "0".repeat(fill)
+  if x < 0  : return binary(int.high+x+1,fill)
+  result = ""
+  var x = x
+  while x > 0:
+    result .add chr('0'.ord + x mod 2)
+    x = x div 2
+  for i in 0..<result.len div 2: swap(result[i],result[result.len-1-i])
+  if result.len >= fill: return result[^fill..^1]
+  return "0".repeat(0.max(fill - result.len)) & result
+proc dump(self:BinPatriciaNode,indent:int = 0) : string =
+  if self == nil : return ""
+  result = ""
+  result .add self.valueOrMask.binary(60)
+  result .add "\t"
+  result .add $self.count
+  for i in 0..<indent: result .add "  "
+  if self.isLeaf: result.add "_"
+  else: result.add "|"
+  result = result & "\n"
+  if self.isLeaf: return
+  if self.to0 != nil: result.add self.to0.dump(indent + 1)
+  if self.to1 != nil: result.add self.to1.dump(indent + 1)
+proc dump(self:BinPatricia) : string = self.root.dump()
+
+
+#
+# 中間点を生成
+proc createInternalNode(now:BinPatriciaNode,preTree:BinPatriciaNode,n:int) : BinPatriciaNode =
+  let cross = preTree.valueOrMask xor n
+  # 頑張れば bit 演算にできそう.
+  for bit in (now.bitSize() - 1).countdown(0):
+    if (cross and (1 shl bit)) == 0 : continue
+    var newLeaf = newBinPatriciaNode()
+    newLeaf.isLeaf = true
+    newLeaf.valueOrMask = n
+    newLeaf.count = 1
+    var created = newBinPatriciaNode()
+    created.isLeaf = false
+    let n1 = preTree.valueOrMask
+    let n2 = newLeaf.valueOrMask
+    let n3 = 1 shl (n1 xor n2).culonglong.fastLog2()
+    created.valueOrMask = (n1 or n2) and (not (n3 - 1))
+    created.count = newLeaf.count + preTree.count
+    if created.isTo1(newLeaf.valueOrMask):
+      created.to1 = newLeaf
+      created.to0 = preTree
+    else:
+      created.to1 = preTree
+      created.to0 = newLeaf
+    return created
+  echo "このメッセージはでないはずだよ"
+  echo n.binary(60)
+  echo now.dump()
+  echo preTree.dump()
+  doAssert false
+
+proc createNextNode(now:BinPatriciaNode,target:BinPatriciaNode,n:int) : BinPatriciaNode =
+  if target == nil:
+    # この先が空なので直接葉を作る
+    result = newBinPatriciaNode()
+    result.isLeaf = true
+    result.valueOrMask = n
+    result.count = 1
+    return
+  if target.isLeaf:
+    if target.valueOrMask == n: # 葉があった
+      target.count += 1
+      return target
+    else: # 中間点を作る
+      return createInternalNode(now,target,n)
+  # prefix が違ったので新しくそこに作る
+  let x = target.valueOrMask
+  let mask = not(x xor (x - 1))
+  if (x and mask) != (n and mask) :
+    return createInternalNode(now,target,n)
+  # 同じprefix を持つのでそちらに進む
+  return nil
+
+# multi-set 的な add
+proc addMulti*(self:var BinPatricia,n:int) =
+  var now = self.root
+  var target : BinPatriciaNode = nil
+  while true:
+    now.count += 1
+    if (now.valueOrMask and n) == now.valueOrMask :
+      target = now.to1
+      let created = createNextNode(now,target,n)
+      if created != nil :
+        now.to1 = created
+        return
+    else:
+      target = now.to0
+      let created = createNextNode(now,target,n)
+      if created != nil :
+        now.to0 = created
+        return
+    now = target
+
+proc `in`*(n:int,self:BinPatricia) : bool =
+  var now = self.root
+  while not now.isLeaf:
+    if now.isTo1(n):
+      if now.to1 == nil : return false
+      now = now.to1
+    else:
+      if now.to0 == nil : return false
+      now = now.to0
+  return now.valueOrMask == n
+# multi-set にしたくなければ
+proc add*(self:var BinPatricia,n:int) =
+  if not (n in self): self.addMulti(n)
+
+import times
+template stopwatch(body) = (let t1 = cpuTime();body;stderr.writeLine "TIME:",(cpuTime() - t1) * 1000,"ms")
+import "../mathlib/random"
+var T = newBinPatricia()
+stopwatch:
+  for i in 0..100000:
+    let r = random(1e9.uint)
+    T.addMulti r.int
--- readme.md
+++ readme.md
@@ -5,12 +5,30 @@
 
 ライブラリはご自由にお使いください
 - lib/datastructure
-  - セグメントツリー{1D,2D} , BIT , StarrySkyTree
-  - BinaryHeap , SkewHeap , UnionFind
-  - Stack , Queue, Deque
-- lib/cpp
-  - std::{set,multiset} (min / max / >= がO(log(N)))
-  - std::vector / std::priority_queue : 互換性のため
+  - UnionFind
+  - `queue/` :: Stack, Queue, Deque
+  - `segmenttree/` :: クエリが O(logN) 以下
+    - セグメントツリー{1D,2D} :: 一点更新, 区間取得
+    - BIT :: 一点更新, 区間和
+    - StarrySkyTree :: 区間更新(加算),区間取得
+  - `heap/`　:: 追加・削除 O(logN) 以下
+    - BinaryHeap : 最小値 O(1)
+    - SkewHeap : ↑ + マージ O(1)
+    - TODO: 二進パトリシア木
+  - `string/`
+    - ロリハ(通常/軽量) :
+    - SA-IS: 接尾辞配列 O(S)
+    - Z-algorithm: 最長共通接頭辞 O(S)
+    - TODO: トライ木
+  - `seq/`
+    - search : 二分探索 / 三分探索 / lowerBoundの `< <= > >=` 表記
+    - LIS : 最長増加部分列
+    - slidemin : スライド最小値
+    - sequence : arg{min,max} / deduplicate
+    - iteration : 順列 / ペア順列 / 階段 / bitDP
+  - `cpp/`
+    - std::{set,multiset} (min / max / >= がO(log(N)))
+    - std::vector / std::priority_queue : 互換性のため
 - lib/mathlib
   - matrix : 行列(転置,乗算,累乗,加算,単位行列)
   - modint : ModInt (累乗,除算)
@@ -19,31 +37,20 @@
   - radix : 基数(2進数,10進数,ビット演算)
   - count : 数え上げ(nCk,カタラン数,第2種スターリング数,x:ベル数,sternBrocotTree(有理数列挙))
   - random : 乱数(Nim0.13用,時間制限まで乱択)
-- lib/sequence
-  - search : 二分探索 / 三分探索 / lowerBoundの `< <= > >=` 表記
-  - LIS : 最長増加部分列
-  - slidemin : スライド最小値
-  - sequence : arg{min,max} / deduplicate
-  - iteration : 順列 / ペア順列 / 階段 / bitDP
-  - 文字列
-    - rollinghash(loliha) : ロリハ(通常/軽量)
-    - Z-Algorithm : 最長共通接頭辞 O(S)
-    - SA-IS : 接尾辞配列(SA-IS) O(S)
-    - PrefixTree : トライ木
 - lib/graph
-  - Tree(入力を木に,オイラーツアー,LCA(最小共通祖先 構築:O(n),探索:O(log(n)) (深さに依存しない)))
-  - DAG(トポロジカルソート,DAG判定, SCC(強連結成分分解) O(V+E))
-  - MST(最小{無向,x:有向}全域木 O(ElogV))
-  - ShortestPath( 最短経路 : O(ElogE) | 負有り:O(EV) | 全:O(V^3))
-  - Flow(最小費用流 O(FElogV) | 最大流/最小カット O(FE) / O(EV^2) | 二部グラフの最大マッチング O(E))
-  - Link(LowLink(橋/関節点),x:二重{頂点,辺}連結成分分解,x:一筆書き)
-  - AdjMatrix(隣接行列: 補グラフ/変換/最大クリーク(最大独立集合)/(最小)彩色数)
-  - Testgraph(テストケース)
+  - Tree :: 入力を木に,オイラーツアー,最小共通祖先(LCA:探索O(log(n)))
+  - DAG :: トポロジカルソート,DAG判定, 強連結成分分解(SCC:O(V+E))
+  - MST :: 最小全域木 O(ElogV)
+  - ShortestPath :: 最短経路 , O(ElogE) | 負有り:O(EV) | 全:O(V^3)
+  - Flow :: 最小費用流 , O(FElogV) | 最大流/最小カット O(FE),O(EV^2) | 二部グラフの最大マッチング O(E)
+  - Link :: 橋,関節点 ,x:一筆書き(オイラー路)
+  - TODO: AdjMatrix(隣接行列: 補グラフ/変換/最大クリーク(最大独立集合)/(最小)彩色数)
+  - TODO: Testgraph(テストケース)
 - lib/geometry
   - geometry(二次元幾何(複素数))
   - pos(二次元座標,dxdy4,dxdy8,Pos)
 - lib/functions.nim (いつもの + IO)
-- lib/trivial : 使いみちのないもの. テストが無いので動作未保証.
+- lib/garbase : 書き捨てたコード
   - sparsematrix : 疎行列
   - math : フィボナッチ数列の第N項 / 線形回帰(最小二乗法)
   - sequence : countContinuity / toCountTable / toTuple / cmp ...
