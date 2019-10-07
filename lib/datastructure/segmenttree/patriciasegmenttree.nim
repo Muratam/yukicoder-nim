07fc8c5767af50ebfe58be6fea20df26eed7bffb
murata <murata@kmc.gr.jp>
2019-10-07 09:07:37 +0900

    セグツリと化したパトリシア木

--- lib/datastructure/segmenttree/patriciasegmenttree.nim
+++ lib/datastructure/segmenttree/patriciasegmenttree.nim
@@ -1,25 +1,32 @@
 import sequtils
-# 本質的には、 multiset と同じく, 追加・削除・極値系全部 ができる
+# まず multiset と同じく, 追加・削除・キーの最{大,小}値 ができる
 # 非負整数のときのセグツリと化した 2進パトリシア木.
 type
-  PatriciaNode[T] = ref object
-    to0,to1 : PatriciaNode[T]
+  PatriciaSegmentNode[T] = ref object
+    to0,to1 : PatriciaSegmentNode[T]
     isLeaf : bool
     count : int32
     valueOrMask : int # 葉なら value / 枝なら一番下位bitが自身が担当するbit番号.それ以外はprefix
     data : T
-  PatriciaTree[T] = ref object
-    root : PatriciaNode[T]
+  PatriciaSegmentTree[T] = ref object
+    root : PatriciaSegmentNode[T]
     unit : T
     apply*:proc(x,y:T):T
-proc newPatriciaNode*[T](self:PatriciaTree[T]):PatriciaNode[T] =
+proc newPatriciaSegmentNode*[T](self:PatriciaSegmentTree[T]):PatriciaSegmentNode[T] =
   new(result)
   result.data = self.unit
-proc newPatriciaTree*[T](apply:proc(x,y:T):T,unit:T,bitSize:int = 60):PatriciaTree[T] =
+# 葉を生成
+proc newPatriciaSegmentLeaf[T](self:PatriciaSegmentTree[T],n:int,val:T) : PatriciaSegmentNode[T] =
+  new(result)
+  result.isLeaf = true
+  result.valueOrMask = n
+  result.count = 1
+  result.data = val
+proc newPatriciaSegmentTree*[T](apply:proc(x,y:T):T,unit:T,bitSize:int = 60):PatriciaSegmentTree[T] =
   new(result)
   result.unit = unit
   result.apply = apply
-  result.root = result.newPatriciaNode()
+  result.root = result.newPatriciaSegmentNode()
   result.root.count = 0
   result.root.isLeaf = false
   result.root.valueOrMask = 1 shl bitSize
@@ -29,10 +36,10 @@ else:
   proc countTrailingZeroBits(x: culonglong): cint {.importc: "__builtin_ctzll", cdecl.}
   proc fastLog2(x:culonglong):cint = 63 - countLeadingZeroBits(x)
 
-proc bitSize*[T](self:PatriciaNode[T]):int =
+proc bitSize*[T](self:PatriciaSegmentNode[T]):int =
   self.valueOrMask.culonglong.countTrailingZeroBits.int
-proc len*[T](self:PatriciaTree[T]) : int = self.root.count
-proc isTo1*[T](self:PatriciaNode[T],n:int):bool{.inline.} =
+proc len*[T](self:PatriciaSegmentTree[T]) : int = self.root.count
+proc isTo1*[T](self:PatriciaSegmentNode[T],n:int):bool{.inline.} =
   (self.valueOrMask and n) == self.valueOrMask # 上位bitは同じはずなので
 
 # デバッグ用
@@ -48,12 +55,15 @@ proc binary*(x:int,fill:int=0):string = # 二進表示
   for i in 0..<result.len div 2: swap(result[i],result[result.len-1-i])
   if result.len >= fill: return result[^fill..^1]
   return "0".repeat(0.max(fill - result.len)) & result
-proc dump*[T](self:PatriciaNode[T],indent:int = 0) : string =
+proc dump*[T](self:PatriciaSegmentNode[T],indent:int = 0) : string =
   if self == nil : return ""
   result = ""
   result .add self.valueOrMask.binary(60)
   result .add "\t"
   result .add $self.count
+  result .add " "
+  result .add $self.data
+  result .add " "
   for i in 0..<indent: result .add "  "
   if self.isLeaf: result.add "_"
   else: result.add "|"
@@ -61,10 +71,10 @@ proc dump*[T](self:PatriciaNode[T],indent:int = 0) : string =
   if self.isLeaf: return
   if self.to0 != nil: result.add self.to0.dump(indent + 1)
   if self.to1 != nil: result.add self.to1.dump(indent + 1)
-proc dump*[T](self:PatriciaTree[T]) : string = self.root.dump()
+proc dump*[T](self:PatriciaSegmentTree[T]) : string = self.root.dump()
 
 # 完全一致検索
-proc `in`*(n:int,self:PatriciaTree) : bool =
+proc `in`*(n:int,self:PatriciaSegmentTree) : bool =
   var now = self.root
   while not now.isLeaf:
     if now.isTo1(n):
@@ -74,19 +84,17 @@ proc `in`*(n:int,self:PatriciaTree) : bool =
       if now.to0 == nil : return false
       now = now.to0
   return now.valueOrMask == n
+
 # 中間点を生成
-proc createInternalNode[T](self:PatriciaTree[T],now:PatriciaNode,preTree:PatriciaNode,n:int,val:T) : PatriciaNode[T] =
+proc createInternalNode[T](self:PatriciaSegmentTree[T],now:PatriciaSegmentNode,preTree:PatriciaSegmentNode,n:int,val:T) : PatriciaSegmentNode[T] =
   let cross = preTree.valueOrMask xor n
   var bit = 0
   # 頑張れば bit 演算にできそう.
   for bitI in (now.bitSize() - 1).countdown(0):
     if (cross and (1 shl bitI)) == 0 : continue
     bit = bitI
-  var newLeaf = self.newPatriciaNode()
-  newLeaf.isLeaf = true
-  newLeaf.valueOrMask = n
-  newLeaf.count = 1
-  var created = self.newPatriciaNode()
+  var newLeaf = self.newPatriciaSegmentLeaf(n,val)
+  var created = self.newPatriciaSegmentNode()
   created.isLeaf = false
   let n1 = preTree.valueOrMask
   let n2 = newLeaf.valueOrMask
@@ -102,49 +110,81 @@ proc createInternalNode[T](self:PatriciaTree[T],now:PatriciaNode,preTree:Patrici
     created.to0 = newLeaf
   return created
 
-proc `[]=`[T](self:var PatriciaTree[T],n:int,val:T) =
+proc `[]=`[T](self:var PatriciaSegmentTree[T],n:int,val:T) =
   var existsKey = n in self
   var now = self.root
-  var target : PatriciaNode[T] = nil
+  var target : PatriciaSegmentNode[T] = nil
+  var path = @[now]
   while true:
     if not existsKey : now.count += 1
     let nowIsTo1 = now.isTo1(n)
     if nowIsTo1: target = now.to1
     else: target = now.to0
-    var created : PatriciaNode[T] = nil
+    proc backwardFromLeaf(self:var PatriciaSegmentTree[T],updateNow:bool = true) =
+      if updateNow:
+        if nowIsTo1: now.to1 = target
+        else: now.to0 = target
+      path.add target
+      for i in (path.len-1).countdown(0):
+        if path[i].to0 == nil:
+          if path[i].to1 == nil: continue
+          path[i].data = path[i].to1.data
+        elif path[i].to1 == nil:
+          path[i].data = path[i].to0.data
+        else:
+          path[i].data = self.apply(path[i].to0.data,path[i].to1.data)
+      # self.backward(path)
     # ノードを見ていく
     if target == nil:
       # この先が空なので直接葉を作る
-      created = self.newPatriciaNode()
-      created.isLeaf = true
-      created.valueOrMask = n
-      created.count = 1
+      target = self.newPatriciaSegmentLeaf(n,val)
+      self.backwardFromLeaf()
+      return
     elif target.isLeaf:
-      if target.valueOrMask == n: # 葉があった
+      let existsLeaf = target.valueOrMask == n
+      if existsLeaf: # 葉があった
         if not existsKey : target.count += 1
         target.data = val
-        return
-      created = self.createInternalNode(now,target,n,val)
+      else:
+        target = self.createInternalNode(now,target,n,val)
+      self.backwardFromLeaf(not existsLeaf)
+      return
     else:
       let x = target.valueOrMask
       let mask = not(x xor (x - 1))
       # prefix が違ったので新しくそこに作る
       if (x and mask) != (n and mask) :
-        created = self.createInternalNode(now,target,n,val)
-    if created != nil :
-      if nowIsTo1:now.to1 = created
-      else:now.to0 = created
-      return
-    now = target
+        target = self.createInternalNode(now,target,n,val)
+        self.backwardFromLeaf()
+        return
+      path.add target
+      now = target
+# 完全一致
+proc `[]`*[T](self:PatriciaSegmentTree[T],n:int) : T =
+  var now = self.root
+  while not now.isLeaf:
+    if now.isTo1(n):
+      if now.to1 == nil :
+        return self.unit
+      now = now.to1
+    else:
+      if now.to0 == nil :
+        return self.unit
+      now = now.to0
+  if now.valueOrMask == n:
+    return now.data
+  return self.unit
 
 
 import times
 template stopwatch(body) = (let t1 = cpuTime();body;stderr.writeLine "TIME:",(cpuTime() - t1) * 1000,"ms")
 import "../../mathlib/random"
-var T = newPatriciaTree(proc(x,y:int):int = x + y,0)
-T[0b0110] = 10
-T[0b0111] = 20
-T[0b011] = 30
-T[0b0] = 40
+var T = newPatriciaSegmentTree(proc(x,y:string):string = x&y,"")
+T[6] = "A"
+T[3] = "B"
+T[0] = "C"
+T[7] = "D"
+T[7] = "E"
+T[15] = "F"
+T[0] = "G"
 echo T.dump()
-# echo T["aiu"]
