ed6bb60c8e83dca318eba0f63ead01e26c3ddecc
murata <murata@kmc.gr.jp>
2019-10-09 20:10:55 +0900

    excl lh

--- lib/datastructure/set/lighttreap.nim
+++ lib/datastructure/set/lighttreap.nim
@@ -1,25 +1,19 @@
 # https://www.slideshare.net/iwiwi/2-12188757
-# LightTreap: std::set(+kth)を実現. 速度重視.
-# min,max,iter,kth,add,erase,find ができる.
+# LightTreap: std::set + カスタムキー を実現. 速度重視.
+# min,max,iter,add,erase,find ができる.
 # セグツリ機能を付けたければ Treap.nim で!!
+
 import "../../mathlib/random"
 import sequtils
 type LightTreap[T] = ref object
   key: T
   priority*: int32
   left*,right*: LightTreap[T] # left: [-∞..key) / right: [key,∞)
-  count*:int # k番目の最小を取るために必要.
 proc newLightTreap*[T](key:T):LightTreap[T] =
   result = LightTreap[T](
     key: key,
     priority:randomBit(30).int32,
-    count:1,
   )
-proc update[T](self:LightTreap[T]) : LightTreap[T] {.discardable.}=
-  self.count = self.left.len + 1 + self.right.len
-  return self
-proc len*[T](self:LightTreap[T]) : int32 =
-  if self == nil: 0 else: self.count
 proc merge*[T](left,right:LightTreap[T]) : LightTreap[T] =
   # 必ず ∀[left] < ∀[right] の時に呼ばれるという仮定を置いている
   if left == nil: return right
@@ -27,21 +21,21 @@ proc merge*[T](left,right:LightTreap[T]) : LightTreap[T] =
   # 優先度が高い方を根にする
   if left.priority > right.priority:
     left.right = left.right.merge(right)
-    return left.update()
+    return left
   else:
     right.left = left.merge(right.left)
-    return right.update()
+    return right
 proc split*[T](self:LightTreap[T],key:T): tuple[l,r:LightTreap[T]] =
   # 再帰的に切るだけ. (子は必ず優先度が低いので)
   if self == nil: return (nil,nil)
   if key < self.key:
     let s = self.left.split(key)
     self.left = s.r
-    return (s.l,self.update())
+    return (s.l,self)
   else:
     let s = self.right.split(key)
     self.right = s.l
-    return (self.update(),s.r)
+    return (self,s.r)
 proc add*[T](self:var LightTreap[T],item: LightTreap[T]) =
   if self == nil:
     self = item
@@ -55,19 +49,17 @@ proc add*[T](self:var LightTreap[T],item: LightTreap[T]) =
     self.left.add(item)
   else:
     self.right.add(item)
-  self.update()
-proc excl*[T](self:var LightTreap[T],key:T) =
+proc excl*[T](self:var LightTreap[T],key:T) : bool =
   # 自分にさようなら
   if self.key == key:
     self = self.left.merge(self.right)
-    return
+    return true
   elif key < self.key:
-    if self.left == nil : return
-    self.left.excl(key)
+    if self.left == nil : return false
+    return self.left.excl(key)
   else:
-    if self.right == nil : return
-    self.right.excl(key)
-  self.update()
+    if self.right == nil : return false
+    return self.right.excl(key)
 proc findByKey*[T](self:LightTreap[T],key:T) : LightTreap[T]=
   if self.key == key: return self
   if key < self.key:
@@ -101,13 +93,12 @@ iterator items*[T](self:LightTreap[T]) : LightTreap[T] =
 type LightTreapRoot*[T] = ref object
   root*:LightTreap[T]
   multiAdd*:bool
-proc newLightTreapRootWith[T](self:LightTreapRoot[T],treap:LightTreap[T]):LightTreapRoot[T] =
-  result = LightTreapRoot(
-    multiAdd:self.multiAdd,
-    root:treap
+  count*:int
+proc newLightTreapRoot[T](multiAdd:bool = true):LightTreapRoot[T] =
+  result = LightTreapRoot[T](
+    multiAdd:multiAdd,
+    count:0,
   )
-proc newLightTreapRoot*[T](multiAdd:bool = true):LightTreapRoot[T] =
-  result = LightTreapRoot[T](multiAdd:multiAdd)
 proc contains*[T](self:LightTreapRoot,key:T):bool=
   if self.root == nil : return false
   return self.root.findByKey(key) != nil
@@ -115,11 +106,12 @@ proc add*[T](self:var LightTreapRoot[T],key:T) =
   # 無ければ追加.とすることで擬似的に multiadd / add を切り替えられる(multiじゃないときはコストだが)
   if not self.multiAdd and key in self: return
   self.root.add(newLightTreap(key))
-proc excl*[T](self:var LightTreapRoot[T],key:T) =
-  if self.root == nil : return
-  self.root.excl(key)
-proc len*[T](self:LightTreapRoot[T]) : int =
-  if self.root == nil : 0 else: self.root.len
+  self.count += 1
+proc excl*[T](self:var LightTreapRoot[T],key:T) : bool {.discardable.}=
+  if self.root == nil : return false
+  result = self.root.excl(key)
+  if result : self.count -= 1
+proc len*[T](self:LightTreapRoot[T]) : int = self.count
 proc findByKey*[T](self:LightTreapRoot[T],key:T):LightTreapRoot[T]=
   if self.root == nil: return nil
   return self.newLightTreapRootWith(self.root.findByKey(key))
