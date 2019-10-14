8d619f69e1ed20374087ca8b5c550f0d2545492b
murata <murata@kmc.gr.jp>
2019-10-15 02:06:45 +0900

    Fixed Universe Set

--- lib/datastructure/set/fuset.nim
+++ lib/datastructure/set/fuset.nim
@@ -1,7 +1,6 @@
 # https://topcoder.g.hatena.ne.jp/spaghetti_source/20121216/1355652855
-# Fixed Universe Set
-# 爆速な std::set.
-# 制約 : 整数のみしか扱えない.
+# Fixed Universe Set. 探索木.
+# 制約 : 整数のみ.
 # 特徴 : 爆速. 値同士が近いと更に爆速.
 const FU64Bit = true
 when FU64Bit: # 範囲が1e9以内ならfalse (ほんのり速い)
@@ -20,33 +19,36 @@ type FUNode = ref object
 type FUSet = ref object
   root : FUNode
   multi: bool
+  size : int
 proc newFUNode(isLeaf:bool):FUNode = FUNode(isLeaf:isLeaf)
 proc getH(self:FUNode,x:int,r:int):int{.inline.} =
   (x shr (r - FUBit)) and ((1 shl FUBit) - 1)
-proc addImpl(self:FUNode,x:int,r:int,multi:bool = true) =
+proc addImpl(self:FUNode,x:int,r:int,multi:bool = true) : bool =
   let h = self.getH(x,r)
   self.S = self.S or (1 shl h)
   if r <= FUBit : # 最下層
+    result = multi or self.count[h] == 0
     if multi: self.count[h] += 1
     else: self.count[h] = 1
     return
   if self.child[h] == nil:
     self.child[h] = newFUNode(r <= FUBit * 2)
-  self.child[h].addImpl(x,r - FUBit,multi)
+  return self.child[h].addImpl(x,r - FUBit,multi)
 proc findImpl(self:FUNode,x:int,r:int) : bool =
   let h = self.getH(x,r)
   if (self.S and (1 shl h)) == 0 : return false
   if r <= FUBit: return true
   return self.child[h].findImpl(x,r-FUBit)
-proc delImpl(self:FUNode,x:int,r:int) =
+proc delImpl(self:FUNode,x:int,r:int) : bool =
   let h = self.getH(x,r)
-  if (self.S and (1 shl h)) == 0 : return
+  if (self.S and (1 shl h)) == 0 : return false
   if r <= FUBit:  # 最下層
     self.count[h] -= 1
-    if self.count[h] == 0:
+    result = self.count[h] == 0
+    if result:
       self.S = self.S and not (1 shl h)
   else:
-    self.child[h].delImpl(x,r-FUBit)
+    result = self.child[h].delImpl(x,r-FUBit)
     if self.child[h].S == 0:
       self.S = self.S and not (1 shl h)
 # proc getAllImpl(self:FUNode): seq[int] =
@@ -56,32 +58,35 @@ proc newFUSet*(multi:bool):FUSet =
   new(result)
   result.root = newFUNode(false)
   result.multi = multi
+  result.size = 0
 proc add*(self:FUSet,x:int) =
-  self.root.addImpl(x + FUOffset,FUMaxBit,self.multi)
+  let ok = self.root.addImpl(x + FUOffset,FUMaxBit,self.multi)
+  if ok: self.size += 1
 proc contains*(self:FUSet,x:int) : bool =
   self.root.findImpl(x + FUOffset,FUMaxBit)
-proc del*(self:FUSet,x:int) : bool =
-  self.root.delImpl(x + FUOffset,FUMaxBit)
-
+proc del*(self:FUSet,x:int) =
+  let ok = self.root.delImpl(x + FUOffset,FUMaxBit)
+  if ok: self.size -= 1
+proc len*(self:FUSet):int = self.size
 
 import times
 import "../../mathlib/random"
-# block:
-#   template stopwatch(body) = (let t1 = cpuTime();body;stderr.writeLine "TIME:",(cpuTime() - t1) * 1000,"ms")
-#   block:
-#     stopwatch:
-#       var S = newFUSet()
-#       let n = 1e6.int
-#       for i in 0..<n: S.add i
-#   block:
-#     stopwatch:
-#       var S = newFUSet()
-#       let n = 1e6.int
-#       for i in 0..<n: S.add randomBit(30).int
-when isMainModule:
-  import unittest
-  test "FU Bit":
-    var S = newFUSet(true)
-    echo 100 in S
-    for i in 0..<100: S.add i
-    # for i in 0..<110: echo i in S
+block:
+  template stopwatch(body) = (let t1 = cpuTime();body;stderr.writeLine "TIME:",(cpuTime() - t1) * 1000,"ms")
+  block:
+    stopwatch:
+      var S = newFUSet(true)
+      let n = 1e6.int
+      for i in 0..<n: S.add i
+  block:
+    stopwatch:
+      var S = newFUSet(true)
+      let n = 1e6.int
+      for i in 0..<n: S.add randomBit(30).int
+# when isMainModule:
+#   import unittest
+#   test "FU Bit":
+#     var S = newFUSet(true)
+#     echo 100 in S
+#     for i in 0..<100: S.add i
+#     # for i in 0..<110: echo i in S
