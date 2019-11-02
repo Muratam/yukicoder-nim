25ea12dc947a7d210a2e8bd20cbbea7ea591c3dd
murata <murata@kmc.gr.jp>
2019-11-02 16:34:58 +0900

    line

--- atcoder/httf.nim
+++ atcoder/httf.nim
@@ -37,6 +37,70 @@ proc pop*[T](q: var Queue[T]): T {.inline, discardable.} =
   result = q.data[q.rd]
   q.data[q.rd] = default(type(result))
   q.rd = (q.rd + 1) and q.mask
+# Priority Queue
+import algorithm
+type PriorityQueue*[T] = ref object
+  data*: seq[T]
+  cmp*:proc(x,y:T):int
+proc ascending*[T](x,y:T):int = (x - y).int # 最小値
+proc descending*[T](x,y:T):int = (y - x).int # 最大値
+proc `[]`[T](heap: PriorityQueue[T], i: Natural): T {.inline.}= heap.data[i]
+proc siftdown[T](heap: var PriorityQueue[T], startpos, p: int) =
+  var pos = p
+  var newitem = heap[pos]
+  while pos > startpos:
+    let parentpos = (pos - 1) shr 1
+    let parent = heap[parentpos]
+    if 0 <= heap.cmp(newitem, parent): break
+    heap.data[pos] = parent
+    pos = parentpos
+  heap.data[pos] = newitem
+proc siftup[T](heap: var PriorityQueue[T], p: int) =
+  let endpos = len(heap)
+  var pos = p
+  let startpos = pos
+  let newitem = heap[pos]
+  var childpos = 2 * pos + 1
+  while childpos < endpos:
+    let rightpos = childpos + 1
+    if rightpos < endpos and 0 <= heap.cmp(heap[childpos], heap[rightpos]):
+      childpos = rightpos
+    heap.data[pos] = heap[childpos]
+    pos = childpos
+    childpos = 2 * pos + 1
+  heap.data[pos] = newitem
+  siftdown(heap, startpos, pos)
+proc len*[T](heap: PriorityQueue[T]): int = heap.data.len
+proc push*[T](heap: var PriorityQueue[T], item: T) =
+  heap.data.add(item)
+  siftdown(heap, 0, len(heap)-1)
+proc pop*[T](heap: var PriorityQueue[T]): T {.discardable.} =
+  let lastelt = heap.data.pop()
+  if heap.len > 0:
+    result = heap[0]
+    heap.data[0] = lastelt
+    siftup(heap, 0)
+  else:
+    result = lastelt
+proc top*[T](heap: PriorityQueue[T]): T = heap.data[0]
+proc poppush*[T](heap: var PriorityQueue[T], item: T): T =
+  result = heap[0]
+  heap.data[0] = item
+  siftup(heap, 0)
+proc pushpop*[T](heap: var PriorityQueue[T], item: T): T =
+  if heap.len > 0 and 0 > heap.cmp(heap[0], item):
+    swap(item, heap[0])
+    siftup(heap, 0)
+  return item
+proc clear*[T](heap: var PriorityQueue[T]) = heap.data.setLen(0)
+proc toSequence*[T](heap: PriorityQueue[T]): seq[T] = heap.data.sorted(cmp)
+proc `$`*[T](heap: PriorityQueue[T]): string = $heap.toSequence()
+iterator items*[T](heap:PriorityQueue[T]) : T =
+  for v in heap.toSequence(): yield v
+proc newPriorityQueue*[T](cmp:proc(x,y:T):int) : PriorityQueue[T] =
+  new(result)
+  result.data = @[]
+  result.cmp = cmp
 # impl
 import sequtils,algorithm,math,tables,sets,strutils,times
 template stopwatch(body) = (let t1 = cpuTime();body;stderr.writeLine "TIME:",(cpuTime() - t1) * 1000,"ms")
@@ -128,20 +192,27 @@ block: # make mat
     initRobMat[r.pos] = r.dir.fromDir()
 # とりあえず正解を置く
 proc bfs() =
-  type PosKind = tuple[pos:Pos,kind:Block]
-  var q = initQueue[PosKind]()
+  type PosKindDist = tuple[pos:Pos,kind:Block,dist:int]
+  var q = initQueue[PosKindDist]()
   for d in dp4:
-    let next = goal + d
-    if mat[next] != Empty: continue
-    q.add((next,(-d).fromDir))
+    var next = goal + d
+    while mat[next] == Empty:
+      q.add((next,(-d).fromDir,0))
+      next = next + d
   while q.len > 0:
-    let (now,kind) = q.pop()
+    let (now,kind,dist) = q.pop()
     if mat[now] != Empty: continue
     mat[now] = kind
-    for d in dp4:
-      let next = now + d
-      if mat[next] != Empty: continue
-      q.add((next,(-d).fromDir))
+    let dirs =
+      if dist mod 2 == 0 :
+        @[(-1,0),(1,0),(0,-1),(0,1)]
+      else:
+        @[(0,-1),(0,1),(-1,0),(1,0)]
+    for d in dirs:
+      var next = now + d
+      while mat[next] == Empty:
+        q.add((next,(-d).fromDir,dist))
+        next = next + d
 # 各ロボットが通る道だけ残す
 proc simplify() : seq[Robot]=
   var route = newSeqWith(n,newSeqWith(n,false))
