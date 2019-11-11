f9f0fee6b9a0f169c237ddb4f280e4a0022a139a
murata <murata@kmc.gr.jp>
2019-11-11 13:37:12 +0900

    kdTree

--- lib/datastructure/set/kdtree.nim
+++ lib/datastructure/set/kdtree.nim
@@ -1,62 +1,108 @@
 # http://www.prefield.com/algorithm/geometry/KDTree2D.html
+# https://topcoder.g.hatena.ne.jp/spaghetti_source/20120908/1347059626
 # 追加・最近傍探索: O(log N)
-# 削除は未実装
+# 削除は再構築しないので徐々に遅くなっていく.
+# => N回find/eraseしたら根から(平衡なKD木を)再構築とかすると多分 O(logN)になると思います.
 type
   Pos*[T] = tuple[x,y:T]
   KDNode2D[T] = ref object
     pos: Pos[T]
     left,right: KDNode2D[T]
+    sameCount: int
   KDTree2D*[T] = ref object
     root: KDNode2D[T]
     size: int
-proc distance[T](a,b:Pos[T]): T =
-  return abs(a.x - b.x) + abs(a.y - b.y)
 proc newKDNode2D[T](pos:Pos[T]):KDNode2D[T] =
   new(result)
   result.pos = pos
-proc newKDTree2D*[T]():KDTree2D[T] = new(result)
+  result.sameCount = 1
 proc add[T](self:KDNode2D[T],isX:bool,pos:Pos[T]):KDNode2D[T] =
   if self == nil:
     return newKDNode2D(pos)
+  if pos == self.pos:
+    self.sameCount += 1
+    return self
   if (isX and pos.x < self.pos.x) or
      (not isX and pos.y < self.pos.y):
     self.left = self.left.add(not isX,pos)
   else:
     self.right = self.right.add(not isX,pos)
   return self
-proc findNearest[T](self:KDNode2D[T],isX:bool,pos:Pos[T],nowDist:T): tuple[pos:Pos[T],dist:T] =
+proc erase[T](self:KDNode2D[T],isX:bool,pos:Pos[T]) : bool {.discardable.}=
+  if self == nil: return false
+  if self.pos == pos:
+    if self.sameCount > 0:
+      self.sameCount -= 1
+      return true
+    return false
+  if (isX and pos.x < self.pos.x) or
+     (not isX and pos.y < self.pos.y):
+    return self.left.erase(not isX,pos)
+  else:
+    return self.right.erase(not isX,pos)
+proc contains[T](self:KDNode2D[T],isX:bool,pos:Pos[T]) : bool =
+  if self == nil: return false
+  if self.pos == pos:
+    return self.sameCount > 0
+  if (isX and pos.x < self.pos.x) or
+     (not isX and pos.y < self.pos.y):
+    return self.left.contains(not isX,pos)
+  else:
+    return self.right.contains(not isX,pos)
+proc findNearest[T](self:KDNode2D[T],distanceFunc : proc (a,b:Pos[T]): T,isX:bool,pos:Pos[T],nowDist:T): tuple[pos:Pos[T],dist:T] =
   if self == nil: return ((-1.T,-1.T),1e12.T)
-  let dist = distance(self.pos,pos)
   var resDist = nowDist
   var resPos = (0.T,0.T)
-  if dist < resDist:
-    resDist = dist
-    resPos = self.pos
+  if self.sameCount > 0:
+    let dist = distanceFunc(self.pos,pos)
+    if dist < resDist:
+      resDist = dist
+      resPos = self.pos
   let isLeft =
      (isX and pos.x < self.pos.x) or
      (not isX and pos.y < self.pos.y)
   proc update(isLeft:bool) =
     let found =
-      if isLeft : self.left.findNearest(not isX,pos,resDist)
-      else: self.right.findNearest(not isX,pos,resDist)
+      if isLeft : self.left.findNearest(distanceFunc,not isX,pos,resDist)
+      else: self.right.findNearest(distanceFunc,not isX,pos,resDist)
     if found.dist < resDist:
       resPos = found.pos
       resDist = found.dist
   update(isLeft)
-  if (isX and abs(pos.x - self.pos.x) <= resDist) or
-    (not isX and abs(pos.y - self.pos.y) <= resDist):
+  if (isX and abs(pos.x - self.pos.x) < resDist) or
+    (not isX and abs(pos.y - self.pos.y) < resDist):
     update(not isLeft)
   return (resPos,resDist)
+proc newKDTree2D*[T]():KDTree2D[T] = new(result)
+proc len*[T](self:KDTree2D[T]):int = self.size
 proc add*[T](self:KDTree2D[T],pos:Pos[T]) =
   self.root = self.root.add(false,pos)
   self.size += 1
-proc len*[T](self:KDTree2D[T]):int = self.size
-proc findNearest*[T](self:KDTree2D[T],pos:Pos[T]): tuple[pos:Pos[T],dist:T] =
-  self.root.findNearest(false,pos,1e12.T)
+proc erase*[T](self:KDTree2D[T],pos:Pos[T]): bool {.discardable.} =
+  result = self.root.erase(false,pos)
+  if result: self.size -= 1
+proc contains*[T](self:KDTree2D[T],pos:Pos[T]): bool =
+  self.root.contains(false,pos)
+proc findNearest*[T](self:KDTree2D[T],distanceFunc : proc (a,b:Pos[T]):T,pos:Pos[T]): tuple[pos:Pos[T],dist:T] =
+  self.root.findNearest(distanceFunc,false,pos,1e12.T)
+iterator items*[T](self:KDTree2D[T]) : Pos[T] =
+  var nodes = @[self.root]
+  while nodes.len > 0:
+    let now = nodes.pop()
+    if now == nil: continue
+    for i in 0..<now.sameCount: yield now.pos
+    if now.left != nil : nodes.add(now.left)
+    if now.right != nil: nodes.add(now.right)
+proc sqEuclidDistance*[T](a,b:Pos[T]): T = # 円
+  (a.x - b.x) * (a.x - b.x) + (a.y - b.y) * (a.y - b.y)
+proc manhattanDistance*[T](a,b:Pos[T]): T = # ひし形
+  abs(a.x - b.x) + abs(a.y - b.y)
+proc chebyshevDistance*[T](a,b:Pos[T]): T = # 四角
+  abs(a.x - b.x).max(abs(a.y - b.y))
 
 when isMainModule:
   import unittest
-  import times
+  import times,sequtils
   import "../../mathlib/random"
   template stopwatch(body) = (let t1 = cpuTime();body;stderr.writeLine "TIME:",(cpuTime() - t1) * 1000,"ms")
   if false:
@@ -68,11 +114,21 @@ when isMainModule:
     stopwatch:
       for i in 0..<n:
         let pos = (randomBit(20),randomBit(20))
-        sumD += kdTree.findNearest(pos).dist
+        sumD += kdTree.findNearest(chebyshevDistance[int],pos).dist
   test "KDTree":
     var kdTree = newKDTree2D[int]()
     kdTree.add((0,10))
     kdTree.add((1000,50))
     kdTree.add((1000,1050))
     check: kdTree.len == 3
-    check: kdTree.findNearest((900,800)) == ((1000,1050),350)
+    check: kdTree.findNearest(manhattanDistance[int],(900,800)) == ((1000,1050),350)
+    check: not kdTree.erase((-1,-1))
+    check: kdTree.len == 3
+    check: kdTree.erase((1000,1050))
+    check: kdTree.len == 2
+    check: kdTree.findNearest(manhattanDistance[int],(900,800)) == ((1000,50),850)
+    check: (0,10) in kdTree
+    check: (10,10) notin kdTree
+    kdTree.add((0,10))
+    check: kdTree.len == 3
+    echo toSeq(kdTree)
